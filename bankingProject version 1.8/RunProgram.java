7c5e5e29dfce6f89d6a3fe21576bfea5a6885705
PikaDisaster2000 <pikadisaste3r2000@gmail.com>
2025-04-10 14:00:52 +1200

    Banking project 1.8
    Again some drastic measures (as I had to skip a few periods to write this last minute code), but allowed to search withdraw and balance using customer ID (aka Customer number). 
    
    Also added delete account and create account.
    Delete account (as of right now) is working properly as intended.
    there are a few issues when creating an account.

---
+++ bankingProject version 1.8/RunProgram.java
@@ -0,0 +1,20 @@
+
+/**
+ * Write a description of class RunProgram here.
+ *
+ * Joseph Tuitupou 
+ * Version 1.7
+ */
+public class RunProgram
+{
+    /**
+     * Constructor for objects of class RunProgram
+     */
+    public RunProgram() //a stud so that we can make a main and start the program. RunProgram, does nothing.
+    {
+        
+    }
+    public static void main(String[] args) { //the main that allows us to run the program. 
+        BankProgram b = new BankProgram();
+    }
+}
